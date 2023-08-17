e361b330ba5c4818d79f18022b9944b960617e6e
winniemei <wmei91@gmail.com>
2023-08-17 12:02:50 -0400

    see all puppies

--- puppyreact/src/API/index.js
+++ puppyreact/src/API/index.js
@@ -1,3 +1,6 @@
+import AllPlayers from "../components/AllPlayers"
+
+
 const API_URL = "https://fsa-puppy-bowl.herokuapp.com/api/2306-GHP-ET-WEB-FT-SF/players"
 const PLAYER_API_URL = "https://fsa-puppy-bowl.herokuapp.com/api/2306-GHP-ET-WEB-FT-SF/players"
 //fetch api from this url
--- puppyreact/src/components/AllPlayers.jsx
+++ puppyreact/src/components/AllPlayers.jsx
@@ -1,5 +1,35 @@
+import { useState, useEffect } from 'react'
+
 export default function AllPlayers() {
+    const [players, setPlayers] = useState([]);
+    useEffect(() => {
+        async function fetchAllPlayers() {
+            try {
+                const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2306-GHP-ET-WEB-FT-SF/players");
+                const result = await response.json();
+                console.log(result);
+                setPlayers(result.data.players);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchAllPlayers();
+    }, [])
     return (
-    <h1>ALL PLAYERS</h1>
+        <div>
+            <h1>ALL PLAYERS</h1>
+            <div>
+                {
+                    players.map((player) => {
+                        return (
+                            <div>
+                                <h4>{player.name}</h4>
+                                ...whatever you want to display
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
 }
