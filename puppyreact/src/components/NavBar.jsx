499ca8c466c193701582a0b802299848fa957f51
winniemei <wmei91@gmail.com>
2023-08-17 17:31:46 -0400

    get single player to render

--- puppyreact/src/components/NavBar.jsx
+++ puppyreact/src/components/NavBar.jsx
@@ -3,9 +3,8 @@ import {Routes, Route, Link} from "react-router-dom";
 export default function NavBar() {
   return (
     <div id="navbar">
-      <Link to="/">ALL PLAYERS</Link>
+      <Link to="/">HOME</Link>
       <Link to="/NewPlayerForm">ADD NEW PLAYER</Link>
-      <Link to="/SinglePlayer">SINGLE PLAYER</Link>
     </div>
   );
 }
