d250fcd687d086784c961ea1e873f8c024ff8d39
winniemei <wmei91@gmail.com>
2023-08-17 16:28:24 -0400

    Revert "add a pup"
    This reverts commit d7b2c614565796fa9915326e7d8f064d522aec9f.

--- puppyreact/src/components/NewPlayerForm.jsx
+++ puppyreact/src/components/NewPlayerForm.jsx
@@ -1,64 +1,48 @@
 import { useState } from "react";
 
-export default function NewPlayerForm() {
-    const [name, setName] = useState("");
-    const [breed, setBreed] = useState("");
-    const [imageUrl, setImageUrl] = useState("");
-    async function handleSubmit(event) {
-        event.preventDefault();
-        try {
-            const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2306-GHP-ET-WEB-FT-SF/players",
-                {
-                    method: "POST",
-                    body: JSON.stringify({ name, breed, imageUrl }),
-                    headers: { "content-type": "application/json" }
-                });
-            const result = await response.json();
-            console.log(result);
-        } catch (error) {
-            console.error(error);
-        }
+export default function NewPlayerForm({ setToken }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+        const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {method: "POST",}) ;
+        const result = await response.json();
+        console.log(result);
+        setToken(result.token);
+    } catch (error) {
+      setError(error.message);
     }
+  }
 
-    return (
-        <>
-            <h1>ADD NEW PLAYER</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={name}
-                    placeholder="Name"
-                    onChange={(e) =>
-                        setName(e.target.value)
-                    }
-                />
-
-                <input
-                    type="text"
-                    id="breed"
-                    name="breed"
-                    value={breed}
-                    placeholder="Breed"
-                    onChange={(e) =>
-                        setBreed(e.target.value)
-                    }
-                />
-
-                <input
-                    type="url"
-                    id="imageUrl"
-                    name="imageUrl"
-                    value={imageUrl}
-                    placeholder="imageUrl"
-                    onChange={(e) =>
-                        setImageUrl(e.target.value)
-                    }
-                />
-
-                <button>Submit</button>
-            </form>
-        </>
-    );
+  return (
+    <>
+      <h1>Sign Up</h1>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+      { !(username.length >= 8) && <p style={{color: 'red'}}>
+            Username must be at least 8 characters
+        </p>
+      }
+    </>
+  );
 }
